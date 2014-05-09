17a80db238cf6ceca5efe9cb87074c526940b54c
Michael Seaton <mseaton@pih.org>
2014-05-09 16:37:45 -0400

    RA-323 - Remove dependency on emrapi module from appui module

--- omod/src/main/java/org/openmrs/module/appui/AppUiArgumentProvider.java
+++ omod/src/main/java/org/openmrs/module/appui/AppUiArgumentProvider.java
@@ -1,19 +1,12 @@
 package org.openmrs.module.appui;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.lang.StringUtils;
-import org.openmrs.Location;
 import org.openmrs.Patient;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.ProviderService;
 import org.openmrs.module.appframework.context.SessionContext;
 import org.openmrs.module.appframework.feature.FeatureToggleProperties;
-import org.openmrs.module.emrapi.adt.AdtService;
-import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
 import org.openmrs.ui.framework.fragment.FragmentContext;
 import org.openmrs.ui.framework.fragment.FragmentModelConfigurator;
 import org.openmrs.ui.framework.fragment.PossibleFragmentActionArgumentProvider;
@@ -25,6 +18,9 @@ import org.openmrs.ui.framework.page.PossiblePageControllerArgumentProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  *
@@ -42,9 +38,6 @@ public class AppUiArgumentProvider implements PageModelConfigurator, FragmentMod
     
     @Autowired
     PatientService patientService;
-    
-    @Autowired
-    AdtService adtService;
 
     @Autowired
     @Qualifier("featureToggles")
@@ -58,7 +51,6 @@ public class AppUiArgumentProvider implements PageModelConfigurator, FragmentMod
         UiSessionContext sessionContext = new UiSessionContext(locationService, providerService, request);
         pageContext.getModel().addAttribute(SESSION_CONTEXT_ATTR, sessionContext);
         pageContext.getModel().addAttribute("featureToggles", featureToggle);
-        pageContext.getModel().addAttribute("activeVisit", getActiveVisit(request, sessionContext));
         pageContext.getModel().addAttribute("patient", getPatient(request));
     }
 
@@ -91,19 +83,8 @@ public class AppUiArgumentProvider implements PageModelConfigurator, FragmentMod
         UiSessionContext sessionContext = new UiSessionContext(locationService, providerService, request);
         possibleArguments.put(SessionContext.class, sessionContext);
         possibleArguments.put(UiSessionContext.class, sessionContext);
-        possibleArguments.put(VisitDomainWrapper.class, getActiveVisit(request, sessionContext));
         possibleArguments.put(Patient.class, getPatient(request));
     }
-
-    private VisitDomainWrapper getActiveVisit(HttpServletRequest request, UiSessionContext sessionContext) {
-		Patient currentPatient = getPatient(request);
-		if (currentPatient != null) {
-			Location visitLocation = adtService.getLocationThatSupportsVisits(sessionContext.getSessionLocation());
-			return adtService.getActiveVisit(currentPatient, visitLocation);
-		}
-		
-		return null;
-	}
 	
 	private Patient getPatient(HttpServletRequest request) {
 		String patientId = request.getParameter("patientId");
