dcd9bda3c6b83dcb5eca7443f16f744fe88ebeb8
mogoodrich <mgoodrich@pih.org>
2015-04-13 17:12:47 -0400

    session fragment controller getLocation should return an ObjectResult, not null

--- omod/src/main/java/org/openmrs/module/appui/fragment/controller/SessionFragmentController.java
+++ omod/src/main/java/org/openmrs/module/appui/fragment/controller/SessionFragmentController.java
@@ -3,15 +3,19 @@ package org.openmrs.module.appui.fragment.controller;
 import org.openmrs.Location;
 import org.openmrs.api.LocationService;
 import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.fragment.action.FragmentActionResult;
+import org.openmrs.ui.framework.fragment.action.ObjectResult;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class SessionFragmentController {
 	
-	public String setLocation(@RequestParam("locationId") Location location,
+	public FragmentActionResult setLocation(@RequestParam("locationId") Location location,
 	                   @SpringBean("locationService") LocationService locationService, UiSessionContext context) {
 		context.setSessionLocation(location);
-		return null;
+		return new ObjectResult(ConversionUtil.convertToRepresentation(location, Representation.DEFAULT));  // TODO: the callback in header.gsp should actually use this information instead of automatically setting the session location and id
 	}
 	
 }
