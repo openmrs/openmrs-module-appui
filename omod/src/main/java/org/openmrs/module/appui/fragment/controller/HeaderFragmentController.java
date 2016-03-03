c33f10bde8d7664c8e7886dc666169829a50ca14
vishnuraom <vishnumechineni@gmail.com>
2016-03-04 01:30:53 +0530

    referenceapplication.lastSessionLocation cookie not changed

--- omod/src/main/java/org/openmrs/module/appui/fragment/controller/HeaderFragmentController.java
+++ omod/src/main/java/org/openmrs/module/appui/fragment/controller/HeaderFragmentController.java
@@ -14,13 +14,20 @@
 
 package org.openmrs.module.appui.fragment.controller;
 
+import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appframework.domain.Extension;
 import org.openmrs.module.appframework.service.AppFrameworkService;
 import org.openmrs.module.appui.AppUiExtensions;
+import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.openmrs.ui.framework.page.PageRequest;
+import org.springframework.web.bind.annotation.CookieValue;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
@@ -33,7 +40,9 @@ public class HeaderFragmentController {
     private static final String GET_LOCATIONS = "Get Locations";
     private static final String VIEW_LOCATIONS = "View Locations";
 
-    public void controller(@SpringBean AppFrameworkService appFrameworkService, FragmentModel fragmentModel) {
+    public void controller(@SpringBean AppFrameworkService appFrameworkService, FragmentModel fragmentModel,
+                           PageRequest pageRequest, UiSessionContext sessionContext,
+                           @CookieValue(value = "referenceapplication.lastSessionLocation",required = false) Location lastLoginLocation) {
         try {
             Context.addProxyPrivilege(GET_LOCATIONS);
             Context.addProxyPrivilege(VIEW_LOCATIONS);
@@ -44,6 +53,23 @@ public class HeaderFragmentController {
             fragmentModel.addAttribute("configSettings", configSettings);
             List<Extension> userAccountMenuItems = appFrameworkService.getExtensionsForCurrentUser(AppUiExtensions.HEADER_USER_ACCOUNT_MENU_ITEMS_EXTENSION);
             fragmentModel.addAttribute("userAccountMenuItems", userAccountMenuItems);
+
+            Integer sessionLocationId = sessionContext.getSessionLocationId();
+            if(lastLoginLocation.getId() != sessionLocationId ){
+                HttpServletRequest request = pageRequest.getRequest();
+                HttpServletResponse response = pageRequest.getResponse();
+
+                Cookie[] cookies = request.getCookies();
+                if (cookies != null) {
+                    for (int i = 0; i < cookies.length; i++) {
+                        if (cookies[i].getName().equals("referenceapplication.lastSessionLocation")) {
+                            cookies[i].setPath("/openmrs/");
+                            cookies[i].setValue(sessionLocationId.toString());
+                            response.addCookie(cookies[i]);
+                        }
+                    }
+                }
+            }
         } finally {
             Context.removeProxyPrivilege(GET_LOCATIONS);
             Context.removeProxyPrivilege(VIEW_LOCATIONS);
