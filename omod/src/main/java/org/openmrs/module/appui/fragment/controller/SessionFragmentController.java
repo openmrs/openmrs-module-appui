fd6d316f5201ac6cf27774c8d8649245b7ccf708
Gayan Weerakutti <gayanw@wso2.com>
2017-03-27 12:41:13 +0530

    RA-546 update lastSessionLocation cookie when changed from header

--- omod/src/main/java/org/openmrs/module/appui/fragment/controller/SessionFragmentController.java
+++ omod/src/main/java/org/openmrs/module/appui/fragment/controller/SessionFragmentController.java
@@ -2,6 +2,7 @@ package org.openmrs.module.appui.fragment.controller;
 
 import org.openmrs.Location;
 import org.openmrs.api.LocationService;
+import org.openmrs.module.appui.AppUiConstants;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
@@ -10,11 +11,18 @@ import org.openmrs.ui.framework.fragment.action.FragmentActionResult;
 import org.openmrs.ui.framework.fragment.action.ObjectResult;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
 public class SessionFragmentController {
 	
 	public FragmentActionResult setLocation(@RequestParam("locationId") Location location,
-	                   @SpringBean("locationService") LocationService locationService, UiSessionContext context) {
+											@SpringBean("locationService") LocationService locationService,
+											UiSessionContext context, ServletContext servletContext, HttpServletResponse httpResponse) {
 		context.setSessionLocation(location);
+		httpResponse.setHeader("Set-Cookie", AppUiConstants.COOKIE_NAME_LAST_SESSION_LOCATION + "=" + location.getLocationId()
+				+ "; HttpOnly; Path=" + servletContext.getContextPath());
+
 		return new ObjectResult(ConversionUtil.convertToRepresentation(location, Representation.DEFAULT));  // TODO: the callback in header.gsp should actually use this information instead of automatically setting the session location and id
 	}
 	
